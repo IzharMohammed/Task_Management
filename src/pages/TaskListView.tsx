92cb165b8b88d9f5aa60e307d047f68097a9a521
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-01-21 22:29:45 +0530

    Added navbar

--- src/pages/TaskListView.tsx
+++ src/pages/TaskListView.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Navbar from "../component/Navbar"
 
 function TaskListView() {
   return (
-    <div>TaskListView</div>
+    <div className="bg-[#FFF9F9] w-full h-screen">
+        <Navbar />
+    </div>
   )
 }
 
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Route, Routes } from 'react-router'
 import Home from '../pages/Home'
 import TaskListView from '../pages/TaskListView'
